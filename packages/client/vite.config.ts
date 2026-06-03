e5584438c33645cf7105e06da6de7a2b7b31f155
Ivan <ivan.alieksieiev1@gmail.com>
2026-06-03 15:48:18 +0300

    Set-up Shadcn/ui

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
+      "/api": "http://localhost:3000",
       // Routing: /api/hello -> http://localhost:3000/api/hello
-    }
-  }
-})
+    },
+  },
+});
